a9c12b60bc64013230be1986811e58a3d73233c0
PoorFellow <crashoverride947@gmail.com>
2014-11-30 19:22:02 -0500

    Crude javafx tests working

--- src/com/poorfellow/graphing/experimental/Main.java
+++ src/com/poorfellow/graphing/experimental/Main.java
@@ -3,12 +3,21 @@ package com.poorfellow.graphing.experimental;
 
 import com.poorfellow.graphing.experimental.JUNG.JUNG;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -25,8 +34,60 @@ public class Main extends Application {
 
     }
 
+    public Main() {
+
+    }
+
+    public void show() {
+        launch();
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        JUNG.executeFX(primaryStage);
+        dynamicComboBoxCreation(primaryStage);
+        //fxmlComboBoxCreation(primaryStage);
+
     }
+
+    private void dynamicComboBoxCreation(Stage primaryStage) {
+
+        ObservableList<String> comboBoxOptions = FXCollections.observableArrayList("Hello", "Goodbye");
+        ComboBox<String> comboBox = new ComboBox<String>(comboBoxOptions);
+        comboBox.setId("demoComboBox");
+        comboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (comboBox.getSelectionModel().getSelectedItem().equals("Hello")) {
+                    Popup popup = new Popup();
+                    Text popupText = new Text();
+                    popupText.setText("World");
+                    popup.getContent().add(popupText);
+                    popup.show(primaryStage);
+                }
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(comboBox);
+        Scene scene = new Scene(root, 500, 500);
+
+        primaryStage.setTitle("Dummy Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        System.out.println("Primary stage is showing? " + primaryStage.isShowing());
+
+    }
+
+    private void fxmlComboBoxCreation(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("testLayout.fxml"));
+
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage.setTitle("Lets see if I can change this now");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+
 }
---
+++ src/com/poorfellow/graphing/experimental/test/JavaFXTest.java
@@ -0,0 +1,99 @@
+package com.poorfellow.graphing.experimental.test;
+
+import com.poorfellow.graphing.experimental.Main;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.loadui.testfx.GuiTest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.assertTrue;
+import static org.loadui.testfx.Assertions.assertNodeExists;
+import static org.loadui.testfx.Assertions.verifyThat;
+
+/**
+ * Created by David on 11/30/2014.
+ */
+public class JavaFXTest extends GuiTest {
+
+    Popup helloPopup;
+
+    @Test
+    public void testButtonClick() {
+        Button btn = find("#demoButton");
+        click(btn);
+        String postClickText = btn.getText();
+        verifyThat("Been Clicked", is(postClickText));
+        //assertNodeExists("demoButton");
+    }
+
+    @Test
+    public void testComboBoxSelect() {
+        ComboBox comboBox = find("#demoComboBox");
+        click(comboBox).click("Hello");
+
+
+        assertTrue(helloPopup.isShowing());
+
+    }
+
+    @Override
+    protected Parent getRootNode() {
+        helloPopup = new Popup();
+        Button demoButton = new Button();
+        demoButton.setText("Hello World");
+        demoButton.setId("demoButton");
+        demoButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                demoButton.setText("Been Clicked");
+            }
+        });
+        AnchorPane root = new AnchorPane();
+
+        ObservableList<String> comboBoxOptions = FXCollections.observableArrayList("Hello", "Goodbye");
+        ComboBox<String> comboBox = new ComboBox<String>(comboBoxOptions);
+        comboBox.setId("demoComboBox");
+        comboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (comboBox.getSelectionModel().getSelectedItem().equals("Hello")) {
+                    Text popupText = new Text();
+                    popupText.setText("World");
+                    helloPopup.getContent().add(popupText);
+                    helloPopup.show(root, 250, 250);
+                }
+            }
+        });
+
+        GridPane grid = new GridPane();
+        grid.setVgap(4);
+        grid.setHgap(10);
+        grid.add(demoButton, 1, 1);
+        grid.add(comboBox, 1, 2);
+
+
+        root.getChildren().addAll(grid);
+
+        return root;
+    }
+}
